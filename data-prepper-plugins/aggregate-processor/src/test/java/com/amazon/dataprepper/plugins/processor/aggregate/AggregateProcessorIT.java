7b965616f6624a2f81491d3c868a7a5fb4a8cb4a
graytaylor0 <tylgry@amazon.com>
2022-02-12 12:35:23 -0600

    Attempt ThreadWeaver
    Signed-off-by: graytaylor0 <tylgry@amazon.com>

--- data-prepper-plugins/aggregate-processor/src/test/java/com/amazon/dataprepper/plugins/processor/aggregate/AggregateProcessorIT.java
+++ data-prepper-plugins/aggregate-processor/src/test/java/com/amazon/dataprepper/plugins/processor/aggregate/AggregateProcessorIT.java
@@ -10,29 +10,27 @@ import com.amazon.dataprepper.model.record.Record;
 import com.amazon.dataprepper.plugins.processor.aggregate.actions.RemoveDuplicatesAggregateAction;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.code.tempusfugit.concurrency.ConcurrentRule;
-import com.google.code.tempusfugit.concurrency.RepeatingRule;
-import com.google.code.tempusfugit.concurrency.annotations.Concurrent;
-import com.google.code.tempusfugit.concurrency.annotations.Repeating;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.google.testing.threadtester.AnnotatedTestRunner;
+import com.google.testing.threadtester.ThreadedAfter;
+import com.google.testing.threadtester.ThreadedBefore;
+import com.google.testing.threadtester.ThreadedMain;
+import com.google.testing.threadtester.ThreadedSecondary;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -40,13 +38,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class AggregateProcessorIT {
-    @Rule
-    public ConcurrentRule rule = new ConcurrentRule();
-
-    @Rule
-    public RepeatingRule repeatingRule = new RepeatingRule();
 
     @Mock
     private static PluginFactory pluginFactory;
@@ -55,20 +48,19 @@ public class AggregateProcessorIT {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<Map<String, Object>>() {};
 
-    private static AggregateProcessor aggregateProcessor;
-    private static AggregateProcessorConfig aggregateProcessorConfig;
-    private static AggregateAction aggregateAction;
-    private static PluginMetrics pluginMetrics;
+    private AggregateProcessor aggregateProcessor;
+    private AggregateProcessorConfig aggregateProcessorConfig;
+    private AggregateAction aggregateAction;
+    private PluginMetrics pluginMetrics;
 
-    private static Map<String, Object> aggregateConfigMap;
+    private Map<String, Object> aggregateConfigMap;
 
-    private static Collection<Record<Event>> eventBatch;
+    private Collection<Record<Event>> eventBatch;
 
-    private static ConcurrentLinkedDeque<Map<String, Object>> aggregatedResult;
-    private static CyclicBarrier cyclicBarrier;
+    private ConcurrentLinkedDeque<Map<String, Object>> aggregatedResult;
 
-    @BeforeClass
-    public static void setup() {
+    @ThreadedBefore
+    public void setup() {
         final List<String> identificationKeys = new ArrayList<>();
         identificationKeys.add("firstRandomNumber");
         identificationKeys.add("secondRandomNumber");
@@ -92,15 +84,7 @@ public class AggregateProcessorIT {
         aggregatedResult = new ConcurrentLinkedDeque<>();
     }
 
-    /*@Test
-    @Concurrent(count = 10)
-    @Repeating(repetition = 10)
-    public void testCombineActionGivesCorrectResults() {
-    }*/
-
-    @Test
-    @Concurrent(count = 10)
-    @Repeating(repetition = 1)
+    @ThreadedMain
     public void testRemoveDuplicatesActionGivesCorrectResults() {
         final List<Record<Event>> recordsOut = (List<Record<Event>>) aggregateProcessor.doExecute(eventBatch);
         for (final Record<Event> record : recordsOut) {
@@ -109,8 +93,17 @@ public class AggregateProcessorIT {
         }
     }
 
-    @AfterClass
-    public static void testAfterAllThreadsRun() {
+    @ThreadedSecondary
+    public void testRemoveDuplicatesActionGivesCorrectResults2() {
+        final List<Record<Event>> recordsOut = (List<Record<Event>>) aggregateProcessor.doExecute(eventBatch);
+        for (final Record<Event> record : recordsOut) {
+            final Map<String, Object> map = record.getData().toMap();
+            aggregatedResult.add(map);
+        }
+    }
+
+    @ThreadedAfter
+    public void testAfterAllThreadsRun() {
         assertThat(aggregatedResult.size(), equalTo(8));
     }
 
@@ -135,5 +128,21 @@ public class AggregateProcessorIT {
         eventMap.put("secondRandomNumber", new Random().nextInt(2));
         eventMap.put("thirdRandomNumber", new Random().nextInt(2));
         return eventMap;
+    }
+
+    @Test
+    public void testPutIfAbsent() throws Exception {
+        System.out.printf("In testPutIfAbsent\n");
+        // Create an AnnotatedTestRunner that will run the threaded tests defined in this
+        // class. We want to test the behaviour of the private method "putIfAbsentInternal" so
+        // we need to specify it by name using runner.setMethodOption()
+        AnnotatedTestRunner runner = new AnnotatedTestRunner();
+        runner.setDebug(true);
+        try {
+            runner.runTests(this.getClass(), AggregateProcessor.class);
+        } catch (Exception e) {
+            throw (Exception) e.getCause();
+        }
+
     }
 }
