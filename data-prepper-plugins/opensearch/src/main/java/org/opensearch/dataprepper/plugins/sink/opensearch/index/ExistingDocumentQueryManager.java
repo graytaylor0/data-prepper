4413b4d765f6189085805b6bea0070d4cb86954f
Taylor Gray <tylgry@amazon.com>
2025-07-02 14:36:38 -0500

    Increase size of search response for querying for duplicates
    Signed-off-by: Taylor Gray <tylgry@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/ExistingDocumentQueryManager.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/ExistingDocumentQueryManager.java
@@ -197,7 +197,7 @@ public class ExistingDocumentQueryManager implements Runnable {
                     m.searches(s -> s
                             .header(h -> h.index(index))
                             .body(b -> b
-                                    .size(chunk.size())
+                                    .size(chunk.size() * 2)
                                     .source(source -> source.filter(f -> f.includes(queryTerm)))
                                     .query(Query.of(q -> q
                                             .terms(TermsQuery.of(t -> t
